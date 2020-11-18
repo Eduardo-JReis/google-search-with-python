38c64b522e7a598395d7270d36124501045d52a0
Eduardo-JReis <edu.publicidade81@gmail.com>
2020-11-17 23:21:47 -0300

    new google_search file

--- src/google_search.py
+++ src/google_search.py
@@ -10,3 +10,16 @@ query = "python Machine Learning"
 
 for j in search(query, num=10, stop=10, pause=2):
     print(j)
+
+# Returned URLs
+
+# https://www.coursera.org/learn/machine-learning-with-python
+# https://www.coursera.org/learn/machine-learning-with-python#about
+# https://www.coursera.org/learn/machine-learning-with-python#syllabus
+# https://www.coursera.org/learn/machine-learning-with-python#reviews
+# https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# https://machinelearningmastery.com/machine-learning-with-python/
+# https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
+# https://www.w3schools.com/python/python_ml_getting_started.asp
+# https://towardsai.net/p/machine-learning/machine-learning-algorithms-for-beginners-with-python-code-examples-ml-19c6afd60daa
+# https://www.edx.org/course/machine-learning-with-python-a-practical-introduct    
